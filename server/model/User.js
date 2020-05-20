d79ed8615c2ad870c125bcffa76134409528fe1b
KangJunewoo <brianjune@naver.com>
2020-05-20 19:34:40 +0900

    basic schema

---
+++ server/model/User.js
@@ -0,0 +1,24 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+  email:{type:String, required:true},
+  password:{type:String, required:true},
+  token:String,
+  agoToken:String,
+  posts:[{type:Schema.Types.ObjectId, ref:'Post'}],
+  recent_login:Date,
+  info:{
+    hours:Schema.Types.Decimal128,
+    questions_answered:Number,
+  },
+  cafe_settings:{
+    theme:{type:String,enum:['a','b','c']},
+    sound_type:{type:String,enum:['a','b','c']},
+    volume:Number
+  },
+  online:Boolean,
+  current_menu:{type:String,enum:['read','write','cafe']},
+});
+
+module.exports = mongoose.model('User', UserSchema);
