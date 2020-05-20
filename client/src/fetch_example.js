0d38151bf522d0bec09ca53a6274bc6d53f7a7fb
KangJunewoo <brianjune@naver.com>
2020-05-20 20:44:11 +0900

    client basic structure

--- client/src/fetch_example.js
+++
@@ -1,9 +0,0 @@
-function fetch_example(){
-  fetch('localhost:3000')
-    .then((res)=>{
-      return res.json();
-    })
-    .then((myJson)=>{
-      console.log(JSON.stringify(myJson));
-    })
-}
