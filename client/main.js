226f620690a3235250af537a9eae05ab1d666189
Taehyeon-Kim <taehyk528@gmail.com>
2020-06-28 17:47:33 +0900

    front-work/키보드 기능 구현: index.html, index.css, renderer.js 추가

--- client/main.js
+++ client/main.js
@@ -1,136 +1,144 @@
-const electron = require('electron');
-const {app, BrowserWindow, ipcMain} = electron;
-const url = require('url');
-const path = require('path');
-const io = require('socket.io-client');
+const electron = require("electron");
+const { app, BrowserWindow, ipcMain } = electron;
+const url = require("url");
+const path = require("path");
+const io = require("socket.io-client");
 
-const axios = require('axios');
+const axios = require("axios");
 const httpInstance = axios.create({
-  baseURL:'http://127.0.0.1:3000'
+  baseURL: "http://127.0.0.1:3000",
 });
 
 // 여기에 들어갈
 // 수많은 import와 이벤트들
-const display_loginwindow = (event,message)=>{
+const display_loginwindow = (event, message) => {
   // 1-1. 옵션 설정해 win창에 담고
-  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
   const options = {
-    width:width,
-    height:height,
-    resizeable:false,
-    fullscreenable:false,
-    show:false,
-    webPreferences:{
-      affinity:true,
-      nodeIntegration:true,
-    }
+    width: 640,
+    height: 1000,
+    resizeable: false,
+    fullscreenable: false,
+    show: false,
+    webPreferences: {
+      affinity: true,
+      nodeIntegration: true,
+    },
   };
   win = new BrowserWindow(options);
 
   // 1-2. html파일 정해주고
-  win.loadURL(url.format({
-    pathname:path.join(__dirname, 'login.html'),
-    protocol:'file',
-    slashes:true,
-  }));
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "login.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
   win.webContents.openDevTools();
 
   // 1-3. 준비완료될때 띄우고 닫았을때 종료해줌.
-  win.once('ready-to-show', ()=>{
-    console.log('ready-to-show');
+  win.once("ready-to-show", () => {
+    console.log("ready-to-show");
     win.show();
   });
-  win.on('closed',()=>{
-    console.log('window closed');
-    win=null;
+  win.on("closed", () => {
+    console.log("window closed");
+    win = null;
     app.quit();
   });
 };
 
-const display_signupmodal = (event,message)=>{
-  console.log('hi');
-  win.webContents.send('hide-page');
-  options={
-    parent:win,
-    modal:true,
-    show:false,
-    webPreferences:{nodeIntegration:true}
-  }
+const display_signupmodal = (event, message) => {
+  console.log("hi");
+  win.webContents.send("hide-page");
+  options = {
+    parent: win,
+    modal: true,
+    show: false,
+    webPreferences: { nodeIntegration: true },
+  };
   modal = new BrowserWindow(options);
 
-  modal.loadURL(url.format({
-    pathname:path.join(__dirname, 'signup_modal.html'),
-    protocol:'file'
-  }));
+  modal.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "signup_modal.html"),
+      protocol: "file",
+    })
+  );
 
-  modal.once('ready-to-show', ()=>{
+  modal.once("ready-to-show", () => {
     modal.show();
   });
-  modal.on('closed', ()=>{
-    modal=null;
+  modal.on("closed", () => {
+    modal = null;
   });
 };
 
-const destroy_signupmodal = (event,message)=>{
-  console.log('merong');
-  win.webContents.send('hide-page');
+const destroy_signupmodal = (event, message) => {
+  console.log("merong");
+  win.webContents.send("hide-page");
   modal.close();
 };
 
-const create_signup_request=(event,message)=>{
-  httpInstance.post('/users', message)
-    .then((response)=>{
-      event.sender.send('signup_request_success', response);
+const create_signup_request = (event, message) => {
+  httpInstance
+    .post("/users", message)
+    .then((response) => {
+      event.sender.send("signup_request_success", response);
     })
-    .catch((error)=>{
-      const result={
-        status:error.response.status,
-        statusText:error.response.statusText
-      }
-      event.sender.send('signup_request_failed', result);
-    })
-};
-
-const create_signin_request=(event,message)=>{
-  httpInstance.post('/users/login', message)
-    .then((response)=>{
-      // TODO : 토큰매니저로 setId
-      event.sender.send('signin_request_success', response);
-    })
-    .catch((error)=>{
+    .catch((error) => {
       const result = {
-        status:error.response.status,
-        statusText:error.response.statusText
+        status: error.response.status,
+        statusText: error.response.statusText,
       };
-      event.sender.send('signin_request_failed',result);
-    })
+      event.sender.send("signup_request_failed", result);
+    });
 };
 
-const go_main = (event, message)=>{
+const create_signin_request = (event, message) => {
+  httpInstance
+    .post("/users/login", message)
+    .then((response) => {
+      // TODO : 토큰매니저로 setId
+      event.sender.send("signin_request_success", response);
+    })
+    .catch((error) => {
+      const result = {
+        status: error.response.status,
+        statusText: error.response.statusText,
+      };
+      event.sender.send("signin_request_failed", result);
+    });
+};
+
+const go_main = (event, message) => {
   win.webContents.clearHistory();
-  win.resizable=true;
-  win.fullScreenable=true;
-  win.setMinimumSize(600,600);
-  win.loadURL(url.format({
-    pathname:path.join(__dirname, 'main.html'),
-    protocol:'file',
-    slashes:true,
-  }));
-  win.once('ready-to-show', ()=>{
+  win.resizable = false;
+  win.fullScreenable = true;
+  win.setMinimumSize(640, 1000);
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "main.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+  win.once("ready-to-show", () => {
     win.show();
   });
-}
+};
 
-app.on('ready', display_loginwindow);
+app.on("ready", display_loginwindow);
 
-ipcMain.on('display_signupmodal', display_signupmodal);
-ipcMain.on('destroy_signupmodal', destroy_signupmodal);
-ipcMain.on('signup_request', create_signup_request);
-ipcMain.on('signin_request', create_signin_request);
-ipcMain.on('go_main', go_main);
-app.on('window-all-closed', ()=>{
+ipcMain.on("display_signupmodal", display_signupmodal);
+ipcMain.on("destroy_signupmodal", destroy_signupmodal);
+ipcMain.on("signup_request", create_signup_request);
+ipcMain.on("signin_request", create_signin_request);
+ipcMain.on("go_main", go_main);
+app.on("window-all-closed", () => {
   app.quit();
-})
-app.on('activate', ()=>{
+});
+app.on("activate", () => {
   app.quit();
 });
---
+++ front-work/js/renderer.js
@@ -0,0 +1,474 @@
+/*--- 키보드 동작 제어 ---*/
+
+document.addEventListener("keydown", function (e) {
+  var 사각형_26 = document.querySelector(".사각형_26");
+  var 사각형_27 = document.querySelector(".사각형_27");
+  var 사각형_28 = document.querySelector(".사각형_28");
+  var 사각형_29 = document.querySelector(".사각형_29");
+  var 사각형_30 = document.querySelector(".사각형_30");
+  var 사각형_31 = document.querySelector(".사각형_31");
+  var 사각형_32 = document.querySelector(".사각형_32");
+  var 사각형_33 = document.querySelector(".사각형_33");
+  var 사각형_34 = document.querySelector(".사각형_34");
+  var 사각형_35 = document.querySelector(".사각형_35");
+  var 사각형_36 = document.querySelector(".사각형_36");
+  var 사각형_37 = document.querySelector(".사각형_37");
+  var 사각형_38 = document.querySelector(".사각형_38");
+  var 사각형_39 = document.querySelector(".사각형_39");
+  var 사각형_40 = document.querySelector(".사각형_40");
+  var 사각형_41 = document.querySelector(".사각형_41");
+  var 사각형_42 = document.querySelector(".사각형_42");
+  var 사각형_43 = document.querySelector(".사각형_43");
+  var 사각형_44 = document.querySelector(".사각형_44");
+  var 사각형_45 = document.querySelector(".사각형_45");
+  var 사각형_46 = document.querySelector(".사각형_46");
+  var 사각형_47 = document.querySelector(".사각형_47");
+  var 사각형_48 = document.querySelector(".사각형_48");
+  var 사각형_49 = document.querySelector(".사각형_49");
+  var 사각형_50 = document.querySelector(".사각형_50");
+  var 사각형_51 = document.querySelector(".사각형_51");
+  var 사각형_52 = document.querySelector(".사각형_52");
+  var 사각형_53 = document.querySelector(".사각형_53");
+  var 사각형_54 = document.querySelector(".사각형_54");
+  var 사각형_55 = document.querySelector(".사각형_55");
+  var 사각형_56 = document.querySelector(".사각형_56");
+  var 사각형_57 = document.querySelector(".사각형_57");
+  var 사각형_58 = document.querySelector(".사각형_58");
+  var 사각형_59 = document.querySelector(".사각형_59");
+  var 사각형_60 = document.querySelector(".사각형_60");
+  var 사각형_61 = document.querySelector(".사각형_61");
+  var 사각형_62 = document.querySelector(".사각형_62");
+  var 사각형_63 = document.querySelector(".사각형_63");
+  var 사각형_64 = document.querySelector(".사각형_64");
+  var 사각형_65 = document.querySelector(".사각형_65");
+  var 사각형_66 = document.querySelector(".사각형_66");
+  var 사각형_67 = document.querySelector(".사각형_67");
+  var 사각형_68 = document.querySelector(".사각형_68");
+  var 사각형_69 = document.querySelector(".사각형_69");
+  var 사각형_70 = document.querySelector(".사각형_70");
+  var 사각형_71 = document.querySelector(".사각형_71");
+  var 사각형_72 = document.querySelector(".사각형_72");
+  var 사각형_73 = document.querySelector(".사각형_73");
+  var 사각형_74 = document.querySelector(".사각형_74");
+  var 사각형_75 = document.querySelector(".사각형_75");
+  var 사각형_76 = document.querySelector(".사각형_76");
+  var 사각형_77 = document.querySelector(".사각형_77");
+  var 사각형_78 = document.querySelector(".사각형_78");
+  var 사각형_79 = document.querySelector(".사각형_79");
+  var 사각형_80 = document.querySelector(".사각형_80");
+  var 사각형_81 = document.querySelector(".사각형_81");
+  var 사각형_82 = document.querySelector(".사각형_82");
+  var 사각형_83 = document.querySelector(".사각형_83");
+  var 사각형_84 = document.querySelector(".사각형_84");
+  var 사각형_85 = document.querySelector(".사각형_85");
+  var 사각형_86 = document.querySelector(".사각형_86");
+  var 사각형_87 = document.querySelector(".사각형_87");
+  var 사각형_88 = document.querySelector(".사각형_88");
+  var 사각형_89 = document.querySelector(".사각형_89");
+  var 사각형_90 = document.querySelector(".사각형_90");
+  var 사각형_91 = document.querySelector(".사각형_91");
+  var 사각형_92 = document.querySelector(".사각형_92");
+  var 사각형_93 = document.querySelector(".사각형_93");
+  var 사각형_94 = document.querySelector(".사각형_94");
+  var 사각형_95 = document.querySelector(".사각형_95");
+  var 사각형_96 = document.querySelector(".사각형_96");
+  var 사각형_97 = document.querySelector(".사각형_97");
+  var 사각형_98 = document.querySelector(".사각형_98");
+  var 사각형_99 = document.querySelector(".사각형_99");
+  var 사각형_100 = document.querySelector(".사각형_100");
+  var 사각형_101 = document.querySelector(".사각형_101");
+  var 사각형_102 = document.querySelector(".사각형_102");
+  var 사각형_103 = document.querySelector(".사각형_103");
+
+  const keyCode = e.keyCode;
+  //console.log("pushed key " + e.key);
+  console.log(keyCode);
+
+  // line 2
+  if (keyCode == 192) {
+    사각형_26.classList.add("tg");
+    setTimeout(function () {
+      사각형_26.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 49) {
+    사각형_27.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_27.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 50) {
+    사각형_28.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_28.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 51) {
+    사각형_29.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_29.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 52) {
+    사각형_30.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_30.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 53) {
+    사각형_31.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_31.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 54) {
+    사각형_32.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_32.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 55) {
+    사각형_33.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_33.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 56) {
+    사각형_34.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_34.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 57) {
+    사각형_35.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_35.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 48) {
+    사각형_36.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_36.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 189) {
+    사각형_37.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_37.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 187) {
+    사각형_38.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_38.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 8) {
+    사각형_39.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_39.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 9) {
+    사각형_40.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_40.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 81) {
+    사각형_41.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_41.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 87) {
+    사각형_42.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_42.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 69) {
+    사각형_43.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_43.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 82) {
+    사각형_44.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_44.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 84) {
+    사각형_45.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_45.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 89) {
+    사각형_46.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_46.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 85) {
+    사각형_47.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_47.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 73) {
+    사각형_48.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_48.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 79) {
+    사각형_49.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_49.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 80) {
+    사각형_50.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_50.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 219) {
+    사각형_51.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_51.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 221) {
+    사각형_52.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_52.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 220) {
+    사각형_53.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_53.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 65) {
+    사각형_54.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_54.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 83) {
+    사각형_55.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_55.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 68) {
+    사각형_56.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_56.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 70) {
+    사각형_57.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_57.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 71) {
+    사각형_58.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_58.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 72) {
+    사각형_59.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_59.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 74) {
+    사각형_60.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_60.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 75) {
+    사각형_61.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_61.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 76) {
+    사각형_62.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_62.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 186) {
+    사각형_63.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_63.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 222) {
+    사각형_64.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_64.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 20) {
+    사각형_65.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_65.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 13) {
+    사각형_66.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_66.classList.remove("tg");
+    }, 100);
+  }
+  // line5
+  else if (keyCode == 90) {
+    사각형_67.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_67.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 88) {
+    사각형_68.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_68.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 67) {
+    사각형_69.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_69.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 86) {
+    사각형_70.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_70.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 66) {
+    사각형_71.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_71.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 78) {
+    사각형_72.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_72.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 77) {
+    사각형_73.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_73.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 188) {
+    사각형_74.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_74.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 190) {
+    사각형_75.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_75.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 191) {
+    사각형_76.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_76.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 16) {
+    사각형_77.classList.toggle("tg");
+    사각형_78.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_77.classList.remove("tg");
+      사각형_78.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 13) {
+    사각형_79.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_79.classList.remove("tg");
+    }, 100);
+  }
+  // line 6
+  else if (keyCode == 17) {
+    사각형_80.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_80.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 18) {
+    사각형_81.classList.toggle("tg");
+    사각형_85.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_81.classList.remove("tg");
+      사각형_85.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 91) {
+    사각형_82.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_82.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 32) {
+    사각형_83.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_83.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 20) {
+    사각형_84.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_84.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 37) {
+    사각형_86.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_86.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 40) {
+    사각형_87.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_87.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 39) {
+    사각형_88.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_88.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 38) {
+    사각형_89.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_89.classList.remove("tg");
+    }, 100);
+  }
+  // line 1
+  else if (keyCode == 27) {
+    사각형_90.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_90.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 112) {
+    사각형_92.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_92.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 113) {
+    사각형_93.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_93.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 114) {
+    사각형_94.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_94.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 115) {
+    사각형_95.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_95.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 116) {
+    사각형_96.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_96.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 117) {
+    사각형_97.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_97.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 118) {
+    사각형_98.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_98.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 119) {
+    사각형_99.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_99.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 120) {
+    사각형_100.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_100.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 121) {
+    사각형_101.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_101.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 122) {
+    사각형_102.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_102.classList.remove("tg");
+    }, 100);
+  } else if (keyCode == 123) {
+    사각형_103.classList.toggle("tg");
+    setTimeout(function () {
+      사각형_103.classList.remove("tg");
+    }, 100);
+  }
+});
